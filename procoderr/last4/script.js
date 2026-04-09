61f73020fc63603d4e87e99b0fd267f7e9af48c2
Vranda-Kansal <itzvaniaggarwal12@gmail.com>
2026-04-09 13:25:28 +0530

    output-based

---
+++ procoderr/last4/script.js
@@ -0,0 +1,43 @@
+// let a = 20;
+// function foo() {
+//   console.log(a);
+//   let a = 10;
+// }
+// foo();
+
+// function outer() {
+//   function inner() {
+//     console.log(x);
+//   }
+//   const x = 5;
+//   return inner;
+// }
+// const inner = outer();
+// inner();
+
+// console.log("Start");
+// setTimeout(() => {
+//   console.log("Timeout");
+// }, 0);
+// console.log("End");
+
+// for (var i = 1; i <= 3; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// for (var i = 1; i <= 3; i++) {
+//   function inner(a) {
+//     setTimeout(() => {
+//       console.log(a);
+//     }, 1000);
+//   }
+//   inner(i);
+// }
+
+// for (let i = 1; i <= 3; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
